5945387fb7e4bf4b437c42ff1a940e3289bbfb90
Kandasamy Murugan <kandu069@gmail.com>
2019-08-14 11:49:39 +0530

    intiial to master branch

---
+++ 02-print.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo Hello World 
+
+## How to print mutliple line with echo command
+echo Hello from Line1
+echo Hello from Line2
+
+echo Hello from Line1\nHello from Line2
+
+echo -e "Hello from Line1\nHello from Line2"
+echo -e "Hello Kanda\nHello Priya\nHello Dhamu"
+echo -e "1.\tHello Kanda\n2.\tHello Dhanu"
+
+echo -e "1.\tKanda\t\tDevops\n2.\tDhamu\t\tBangalore"
+
+echo -e "\e[33myellow forground color\e[0m"
+
+echo -e "\e[35mWelcome Kanda for Devops Course\e[0m"
+
+echo -e "\e[34mKadayanallur is the Beautiful Ciry \e[0m"
+
+echo -e "\e[36mIndia is my country and I am proud of it\e[0m"
+
+echo "No color required for this statement"
+
+echo -e "\e[43;31mREd on yello\e[0m"
+
+echo -e "\e[34;31mKandasamy Murugan\tChennai\e[0m"
+
+echo -e "\e[33;34m1.Name Kandasamy\n2.Age 32\e[0m"
+
+echo -e "\e[35m1.Name\t:Dhamu\n2.Company\t:Aspire\e[33m\n3.Place\t:Chennai\e[32m\n4.Exp\t:4years\e[0m"
+
+echo "this is for small change"
