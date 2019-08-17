bdf62f3eb7cf29cdb65a2b6dbe570adcde37b043
Kandasamy Murugan <kandu069@gmail.com>
2019-08-17 09:29:34 +0530

    ADDING TO VARIABLE1

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,3 +3,13 @@ Count=10
 Class=DevOps
 
 echo "Welcome to $Class Training and Strength is $Count"
+
+echo "$Class Training"
+
+echo "Today date is 2019-08-12"
+
+DATE=$(date +%F)
+echo "Today date is $DATE"
+
+ADD=$((2+3))
+echo "ADD of 2 and 3 = $ADD"
